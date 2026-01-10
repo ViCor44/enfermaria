fa83baedee11c90a352a64fecf8d9f3776ff9686
ViCor44 <victor.a.correia@gmail.com>
2026-01-10 10:46:01 +0000

    alterações ssd

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -332,7 +332,7 @@ class AuthController
 
         // 1️⃣ Validar token
         $stmt = $pdoSuper->prepare("
-            SELECT admin_id, system_id
+            SELECT admin_id, system_key
             FROM admin_tokens
             WHERE token = ?
             AND used = 0
@@ -356,9 +356,9 @@ class AuthController
         $stmt = $pdoSuper->prepare("
             SELECT user_id
             FROM admin_user_map
-            WHERE admin_id = ? AND system_id = ?
+            WHERE admin_id = ? AND system_key = ?
         ");
-        $stmt->execute([$t['admin_id'], $t['system_id']]);
+        $stmt->execute([$t['admin_id'], $t['system_key']]);
         $map = $stmt->fetch();
 
         if (!$map) {
