bed7cfcb0fcc83f8cf1e3d16874c8b1740f91d1c
ViCor44 <victor.a.correia@gmail.com>
2026-05-05 20:27:37 +0100

    chore: remover opcache reset helper

--- public/opcache_reset.php
+++
@@ -1,9 +0,0 @@
-<?php
-// APAGAR ESTE FICHEIRO APÓS USO
-// Aceder via browser: /enfermaria/public/opcache_reset.php
-if (function_exists('opcache_reset')) {
-    opcache_reset();
-    echo 'OPcache limpo com sucesso.';
-} else {
-    echo 'OPcache não está ativo.';
-}
